7266dc73af2b241b5a16a1f7f5d17862c3e57dfd
Ole Martin Pettersen <olemp@puzzlepart.com>
2018-02-02 15:19:26 +0100

    Updated utils\powershell

--- .tasks/utils/powershell.js
+++ .tasks/utils/powershell.js
@@ -1,12 +1,10 @@
 'use strict';
 var exec = require('child_process').exec,
     format = require('string-format'),
-    config = require("../@configuration.js");
+    configuration = require("../@configuration.js");
 
 var exports = module.exports = {};
 
-exports.execute = function (filePath, args, done) {
-    exec(format("Powershell.exe  -executionpolicy remotesigned -File {0}\\{1} {2}", config.paths.scripts, filePath, args), () => {
-        done();
-    });
+exports.execute = (filePath, args, done) => {
+    exec(format("Powershell.exe  -executionpolicy remotesigned -File {0}\\{1} {2}", configuration.PATHS.SCRIPTS, filePath, args), done);
 }
