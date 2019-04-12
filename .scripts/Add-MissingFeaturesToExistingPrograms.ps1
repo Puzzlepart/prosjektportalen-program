08ee15350593b057a88880fb88a628b05ef3dbaa
Stian Grepperud <stian.grepperud@puzzlepart.com>
2019-04-12 13:38:08 +0200

    Updating navigation and added progressbar

--- .scripts/Add-MissingFeaturesToExistingPrograms.ps1
+++ .scripts/Add-MissingFeaturesToExistingPrograms.ps1
@@ -1,4 +1,4 @@
-Param(
+﻿Param(
     [Parameter(Mandatory = $true)]
     [string]$Url,
     [Parameter(Mandatory = $false, HelpMessage = "Use current credentials?")]
@@ -7,74 +7,79 @@ Param(
     [switch]$UseWebLogin
 )
 
-function Connect($Url) {
-    if ($CurrentCredentials.IsPresent) {
-        Connect-PnPOnline -Url $Url -CurrentCredentials
-    }
-    elseif ($UseWebLogin.IsPresent) {
-        Connect-PnPOnline -Url $Url -UseWebLogin
-    }
-    else {
-        Connect-PnPOnline -Url $Url
-    }
-}
-
 <# Add ProgramProjectStats #>
-function Add-MissingWebpart($Web, $WebpartName, $RootWeb) {
+function Add-MissingWebpart($Web, $WebpartName, $WebpartFilename, $RootWeb) {
     $serverRelativeUrl = $RootWeb.ServerRelativeUrl
     $subwebRelativeUrl = $Web.ServerRelativeUrl
-
-    if ($WebpartName -eq "DeliveriesOverview") {
-        $targetFileName = "Program-leveranseoversikt.aspx"
-    }
-    else {
-        $targetFileName = "Program$WebpartName.aspx"
-    }
     Try {
-        $file = Get-PnPFile -Url "$subwebRelativeUrl/SitePages/$targetFileName"
+        $file = Get-PnPFile -Url "$subwebRelativeUrl/SitePages/Program$WebpartFilename.aspx" -ErrorAction SilentlyContinue
         if ($null -eq $file) {   
             Copy-PnPFile -SourceUrl "Resources/SitePage_OneColumn.txt" -TargetUrl "$subwebRelativeUrl/SitePages" -OverwriteIfAlreadyExists -Force
-            Rename-PnPFile -ServerRelativeUrl "$subwebRelativeUrl/SitePages/SitePage_OneColumn.txt" -TargetFileName $targetFileName -Force -Web $Web
+            Rename-PnPFile -ServerRelativeUrl "$subwebRelativeUrl/SitePages/SitePage_OneColumn.txt" -TargetFileName "Program$WebpartFilename.aspx" -Force -Web $Web
         }
-        $webparts = Get-PnPWebPart -ServerRelativePageUrl "$subwebRelativeUrl/SitePages/$targetFileName"
+        $webparts = Get-PnPWebPart -ServerRelativePageUrl "$subwebRelativeUrl/SitePages/Program$WebpartFilename.aspx"
         if ($null -ne $webparts) {
             $webparts | ForEach-Object {
-                Remove-PnPWebPart -Identity $_.Id -ServerRelativePageUrl "$subwebRelativeUrl/SitePages/$targetFileName" -Web $Web
+                Remove-PnPWebPart -Identity $_.Id -ServerRelativePageUrl "$subwebRelativeUrl/SitePages/Program$WebpartFilename.aspx" -Web $Web
             }
         }
-        $xml = Get-PnPFile -Url "$serverRelativeUrl/_catalogs/wp/Program$WebpartName.webpart" -AsString
-        $url = "$subwebRelativeUrl/SitePages/$targetFileName"
+        $xml = Get-PnPFile -Url "$serverRelativeUrl/_catalogs/wp/Program$WebpartFilename.webpart" -AsString
+        $url = "$subwebRelativeUrl/SitePages/Program$WebpartFilename.aspx"
         Add-PnPWebPartToWebPartPage -ServerRelativePageUrl $url -Xml $xml -ZoneId "Header" -ZoneIndex 1 -Web $Web
+
+        $navNode = Get-PnPNavigationNode -Web $Web | Where-Object { $_.Title -Match $WebpartName }
+        if ($null -eq $navNode) {
+            $navParent = Get-PnPNavigationNode -Web $Web | Where-Object { $_.Title -Match "Programmets prosjekter" }
+            Add-PnPNavigationNode -Location "QuickLaunch" -Title $WebpartName -Parent $navParent.Id -Url "$subwebRelativeUrl/SitePages/Program$WebpartFilename.aspx" -Web $Web | Out-Null
+        } 
     }
     Catch {
         Write-Host $_.Exception.Message
     }
 }
 
-Connect -Url $Url
+if ($CurrentCredentials.IsPresent) {
+    Connect-PnPOnline -Url $Url -CurrentCredentials
+}
+elseif ($UseWebLogin.IsPresent) {
+    Connect-PnPOnline -Url $Url -UseWebLogin
+}
+else {
+    Connect-PnPOnline -Url $Url
+}
 
-$missingWebParts = @("ProjectStats", "ExperienceLog", "DeliveriesOverview", "RiskOverview", "ResourceAllocation")
+$missingWebParts = @(
+    @{Filename = "ProjectStats"; Name = "Porteføljeinnsikt"}, 
+    @{Filename = "ExperienceLog"; Name = "Erfaringslogg"},
+    @{Filename = "DeliveriesOverview"; Name = "Leveranseoversikt"}, 
+    @{Filename = "RiskOverview"; Name = "Risikooversikt"},
+    @{Filename = "ResourceAllocation"; Name = "Ressursallokering"})
 
 $web = Get-PnPWeb
 
 <# Add Missing SitePages and web parts #>
-Get-PnPSubWebs | ForEach-Object {
+$i = 0
+$subwebs = Get-PnPSubWebs
+Write-Host "Adding missing pages and web parts." -ForegroundColor Green
+$subwebs | ForEach-Object {
+    Write-Progress -Id 1 -Activity "Updating programs" -Status "$i/$($subwebs.Count)" -PercentComplete (($i / $subwebs.Count) * 100)
     $currentWeb = $_
     Try {
-
+        $j = 0
         $missingWebParts | ForEach-Object {
-            $webpartName = $_
             Try {
-                Add-MissingWebpart -Web $currentWeb -WebpartName $webpartName -RootWeb $web
+                Add-MissingWebpart -Web $currentWeb -WebpartName $_.Name -WebpartFilename $_.Filename -RootWeb $web
+                $j++
             }
             Catch {
                 Write-Host "Failed to add missing webpart"
             }
+            Write-Progress -Id 2 -ParentId 1 -Activity "Adding missing pages, web parts and updating navigation" -Status "$j/$($missingWebParts.Count)" -PercentComplete (($j / $missingWebParts.Count) * 100)
         }
     }
     Catch {
         $_.Exception.Message
     }
-    <# Update Navigation #>
-    Apply-PnPProvisioningTemplate -Path ./Navigation.xml -Web $currentWeb
+    $i++
+    Write-Progress -Id 1 -Activity "Updating programs" -Status "$i/$($subwebs.Count)" -PercentComplete (($i / $subwebs.Count) * 100)
 }
